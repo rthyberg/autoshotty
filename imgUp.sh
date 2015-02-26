a29e1d127d970e231e8d7d948207bfcae26fd7f1
Robert Thyberg <robertthyberg@gmail.com>
2015-02-26 01:06:12 -0800

    added bash script to run the py file

---
+++ imgUp.sh
@@ -0,0 +1,31 @@
+#DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  TARGET="$(readlink "$SOURCE")"
+  if [[ $SOURCE == /* ]]; then
+    #   echo "SOURCE '$SOURCE' is an absolute symlink to '$TARGET'"
+    SOURCE="$TARGET"
+  else
+    DIR="$( dirname "$SOURCE" )"
+    # echo "SOURCE '$SOURCE' is a relative symlink to '$TARGET' (relative to '$DIR')"
+    SOURCE="$DIR/$TARGET" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+  fi
+done
+#echo "SOURCE is '$SOURCE'"
+RDIR="$( dirname "$SOURCE" )"
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+if [ "$DIR" != "$RDIR" ]; then
+#  echo "DIR '$RDIR' resolves to '$DIR'"
+fi
+# echo "DIR is '$DIR'"
+
+
+
+
+
+
+source $DIR/testing/bin/activate
+python $DIR/imgUp_shutter.py $DIR
+deactivate
