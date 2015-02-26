0188f01c94835103fd4ecdb01a65f843c34a67df
Robert Thyberg <robertthyberg@gmail.com>
2015-02-26 02:50:07 -0800

    small changes

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ImgScriptShutter
-Small python script to take a screenshot, upload imgur, copy link to clipboard
+
+Small python script to take a  screenshot with shutter, upload imgur, copy link to clipboard
+
+to use make sure to chmod u+x imgUp.sh
+and export the path into your bashrc
+export PATH="$PATH:~/pathtoscript
+
+This is my first github! WOOT!
--- imgUp_shutter.py
+++ imgUp_shutter.py
@@ -3,20 +3,29 @@ import os
 import subprocess
 import pyimgur
 import pyperclip
-# refer to man pages for what this does
-shutter_quick = subprocess.call(["shutter", "-s", "-C", "--output=~/my_scripts/data/scriptshot.png", "--no_session", "--exit_after_capture"])
+import sys
 
+pathname = os.path.dirname(sys.argv[0])
+current_path = os.path.abspath(pathname)
+print(current_path)
+if os.path.isdir(os.path.join(current_path, "data")):
+   current_path = os.path.join(current_path, "data")
 
-# this what im working on now
+screenshot_path = os.path.join(current_path, "scriptshot.png")
+
+# calls shutter with commandline arguments for selection, no cursor, savepath, nosession, exit after capture
+shutter_quick = subprocess.call(["shutter", "-s", "-C","--output={0}".format(screenshot_path) , "--no_session", "--exit_after_capture"])
+
+# register app with imgur
 CLIENT_ID = "56bedd07cb62731"
-PATH = "data/scriptshot.png"
 
+# upload image at our path
 im = pyimgur.Imgur(CLIENT_ID)
-uploaded_image = im.upload_image(PATH, title="Uploaded with my super duper cool script")
+uploaded_image = im.upload_image(screenshot_path, title="Uploaded with my super duper cool script")
 
+# copy the image link to clip board
 pyperclip.copy(uploaded_image.link)
 
-
-subprocess.call(["rm", PATH])
-
+# delete image
+subprocess.call(["rm", screenshot_path])
 
