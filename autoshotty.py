aa6be609b7232679ca364753128a4f1e76934b6d
Robert Thyberg <robertthyberg@gmail.com>
2015-12-13 18:50:12 -0800

    Disabled keyboard canceling, fixed comments and added prints for commandline

--- autoshotty.py
+++ autoshotty.py
@@ -9,8 +9,10 @@ import sys
 # Find home path
 pathtodir = os.path.expanduser('~')
 screenshot_path = pathtodir + "/.scriptshot.png"
-# calls maim with commandline arguments for selection, no cursor, savepath, nosession, exit after capture
-maim_quick = subprocess.call(["maim", "-s", screenshot_path])
+
+# calls maim with commandline arguments for selection, nokeyboard
+print("Select an area with your mouse cursor")
+maim_quick = subprocess.call(["maim", "-s", "--nokeyboard", screenshot_path])
 
 # register app with imgur
 CLIENT_ID = "9ead9b7b006931b"
@@ -22,6 +24,7 @@ uploaded_image = im.upload_from_path(screenshot_path)
 
 # copy the image link to clip board
 pyperclip.copy(uploaded_image['link'])
+print("Now paste away")
 
 # delete image
 subprocess.call(["rm", screenshot_path])
